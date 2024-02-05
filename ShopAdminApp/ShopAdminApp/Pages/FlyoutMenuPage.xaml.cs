cd755de3c3ec24d3436d003d45f9f415f24443f1
Tim <sarrtim@gmail.com>
2024-02-06 01:31:25 +0300

    Add flyout menu navigation

---
+++ ShopAdminApp/ShopAdminApp/Pages/FlyoutMenuPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ShopAdminApp
+{
+	public partial class FlyoutMenuPage : ContentPage
+    {
+		public FlyoutMenuPage()
+		{
+            InitializeComponent();
+        }
+	}
+}
+
---
+++ ShopAdminApp/ShopAdminApp/Pages/FlyoutPageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ShopAdminApp
+{
+	public class FlyoutPageItem
+    {
+        public string Title { get; set; }
+
+        public string IconSource { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
+
