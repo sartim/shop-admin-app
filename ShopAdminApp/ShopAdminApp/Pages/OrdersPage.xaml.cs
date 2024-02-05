946264a8e8970798ace76b24903e9729634a7906
Tim <sarrtim@gmail.com>
2024-02-06 01:33:25 +0300

    Add order page resources

---
+++ ShopAdminApp/ShopAdminApp/Pages/OrdersPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ShopAdminApp
+{
+    public partial class OrdersPage : ContentPage
+    {
+        public OrdersPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
