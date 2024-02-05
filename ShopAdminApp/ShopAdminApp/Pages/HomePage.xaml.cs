28ab5e78dcea38e9c9521642b48a023ffa9c83df
Tim <sarrtim@gmail.com>
2024-02-06 01:32:52 +0300

    Update home page

--- ShopAdminApp/ShopAdminApp/Pages/HomePage.xaml.cs
+++ ShopAdminApp/ShopAdminApp/Pages/HomePage.xaml.cs
@@ -8,11 +8,29 @@ using Xamarin.Forms;
 
 namespace ShopAdminApp
 {
-    public partial class HomePage : ContentPage
+    public partial class HomePage : FlyoutPage
     {
         public HomePage()
         {
             InitializeComponent();
+
+            flyoutPage.listView.ItemSelected += OnItemSelected;
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                FlyoutLayoutBehavior = FlyoutLayoutBehavior.Popover;
+            }
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as FlyoutPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                flyoutPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
         }
     }
 }
