aaf7001ff65e668da71ac7800790e7cff4600cb2
Tim <sarrtim@gmail.com>
2024-02-09 19:49:05 +0300

    Refactor model to match response API

--- ShopAdminApp/ShopAdminApp/Models/Product.cs
+++ ShopAdminApp/ShopAdminApp/Models/Product.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace ShopAdminApp.Models
 {
 	public class Product
 	{
-        public string Name { get; set; }
+        public string count { get; set; }
 
-        public string Category { get; set; }
+        public string next { get; set; }
 
-        public string Brand { get; set; }
+        public string previous { get; set; }
 
-        public string Price { get; set; }
+        public List<ProductDetail> results { get; set; }
+       
+    }
+
+    public class ProductDetail
+    {
+        public string name { get; set; }
+
+        public string count { get; set; }
+
+        public string brand { get; set; }
+
+        public string price { get; set; }
     }
 }
 
