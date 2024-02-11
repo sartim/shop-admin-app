6fc2bd9e9da847dbcbb7ef98d93f054c78398e3a
Tim <sarrtim@gmail.com>
2024-02-11 21:20:48 +0300

    Update to render list view from http results

--- ShopAdminApp/ShopAdminApp/Pages/ProductsPage.xaml.cs
+++ ShopAdminApp/ShopAdminApp/Pages/ProductsPage.xaml.cs
@@ -1,15 +1,41 @@
 ﻿
 using System;
+using Newtonsoft.Json;
+using ShopAdminApp.Models;
+using ShopAdminApp.Services;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ShopAdminApp
 {
 	public partial class ProductsPage : ContentPage
 	{
-		public ProductsPage()
-		{
+        private RestService restService;
+
+        public ProductsPage()
+        {
 			InitializeComponent();
-		}
-	}
+            restService = new RestService();
+            GetProducts();
+        }
+
+        public async void GetProducts()
+        {
+            //Check network status   
+            if (NetworkCheck.IsInternet())
+            {
+                var products = await restService.ListProducts();
+
+                //Binding listview with server response    
+                listviewProducts.ItemsSource = products.results;
+            }
+            else
+            {
+                await DisplayAlert("JSONParsing", "No network is available.", "Ok");
+            }
+            //Hide loader after server response    
+            ProgressLoader.IsVisible = false;
+        }
+    }
 }
 
