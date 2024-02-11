70e7793ba31abb1e119591c23fc4ffafd134971e
Tim <sarrtim@gmail.com>
2024-02-11 21:17:19 +0300

    Add category model

---
+++ ShopAdminApp/ShopAdminApp/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ShopAdminApp.Models
+{
+    public class Category
+    {
+        public string count { get; set; }
+
+        public string next { get; set; }
+
+        public string previous { get; set; }
+
+        public List<CategoryDetail> results { get; set; }
+    }
+
+    public class CategoryDetail
+    {
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public string deleted { get; set; }
+    }
+}
