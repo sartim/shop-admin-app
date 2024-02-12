c72192286787dcca9658c36caa4f6d3c7f93f96c
Tim <sarrtim@gmail.com>
2024-02-12 18:42:41 +0300

    Add pages

---
+++ ShopAdminApp/ShopAdminApp/Pages/CategoryPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShopAdminApp
+{	
+	public partial class CategoryPage : ContentPage
+	{	
+		public CategoryPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
