429530dfbf199ff2f5f153e82a1b98bcacd48dea
Tim <sarrtim@gmail.com>
2024-02-12 18:43:01 +0300

    Add logout

---
+++ ShopAdminApp/ShopAdminApp/Pages/LogoutPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using ShopAdminApp.Services;
+
+namespace ShopAdminApp
+{
+    public partial class LogoutPage : ContentPage
+    {
+        public LogoutPage()
+        {
+            InitializeComponent();
+            Logout();
+        }
+
+        async void Logout()
+        {
+            SecureStorage.Remove("jwt");
+            await Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
+
