8c422c52e7ea007c492a04eb7afbc187a3087d9c
Tim <sarrtim@gmail.com>
2024-02-16 01:17:47 +0300

    Refactor service

---
+++ ShopAdminApp/ShopAdminApp/Services/AuthService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace ShopAdminApp.Services
+{
+    public class AuthService : BaseService
+    {
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var json = $"{{\"email\": \"{email}\", \"password\": \"{password}\"}}";
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _client.PostAsync("api/v1/auth/generate-jwt", content);
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    await SecureStorage.SetAsync("jwt", response.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.WriteLine("Possible that device doesn't support secure storage on device.");
+                }
+            }
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ ShopAdminApp/ShopAdminApp/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ShopAdminApp.Services
+{
+    public class ProductService : BaseService
+    {
+        public async Task<T> GetAsync<T>(string url)
+        {
+            return await SendRequestAsync<T>(HttpMethod.Get, url);
+        }
+
+        public async Task<T> PostAsync<T>(string url, object data)
+        {
+            return await SendRequestAsync<T>(HttpMethod.Post, url, data);
+        }
+
+        public async Task<T> PutAsync<T>(string url, object data)
+        {
+            return await SendRequestAsync<T>(HttpMethod.Put, url, data);
+        }
+
+        public async Task<bool> DeleteAsync(string url)
+        {
+            HttpResponseMessage response = await _client.DeleteAsync(url);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
