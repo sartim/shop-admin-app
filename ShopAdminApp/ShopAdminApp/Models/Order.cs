ecb0ecccb18fe65494adc9f5278a821ffbe8b936
Tim <sarrtim@gmail.com>
2024-02-16 02:18:01 +0300

    Render orders

--- ShopAdminApp/ShopAdminApp/Models/Order.cs
+++ ShopAdminApp/ShopAdminApp/Models/Order.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 namespace ShopAdminApp.Models
 {
-	public class Order
+    public class Order
     {
-        public string ID { get; set; }
+        public string count { get; set; }
+
+        public string next { get; set; }
+
+        public string previous { get; set; }
+
+        public List<OrderDetail> results { get; set; }
+
+    }
+
+    public class OrderDetail
+    {
+        public string id { get; set; }
+
+        public string order_total { get; set; }
+
     }
 }
 
--- ShopAdminApp/ShopAdminApp/Pages/OrdersPage.xaml.cs
+++ ShopAdminApp/ShopAdminApp/Pages/OrdersPage.xaml.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using System.Threading.Tasks;
+using ShopAdminApp.Models;
+using ShopAdminApp.Services;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ShopAdminApp
 {
     public partial class OrdersPage : ContentPage
     {
+        private readonly RestService _restService;
+
         public OrdersPage()
         {
             InitializeComponent();
+            GetOrders();
+        }
+
+        public async void GetOrders()
+        {
+            try
+            {
+                if (NetworkCheck.IsInternet())
+                {
+                    string authToken = await SecureStorage.GetAsync("jwt");
+
+                    // Offload the network request to a background thread
+                    var orders = await Task.Run(() => _restService.GetAsync<Order>("api/v1/orders", authToken));
+
+                    // Update the UI on the main thread
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        //Binding listview with server response    
+                        listviewOrders.ItemsSource = orders.results;
+                    });
+                }
+                else
+                {
+                    await DisplayAlert("JSONParsing", "No network is available.", "Ok");
+                }
+                ProgressLoader.IsVisible = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"Failed to load orders: {ex.Message}", "OK");
+                ProgressLoader.IsVisible = false;
+            }
         }
     }
 }
